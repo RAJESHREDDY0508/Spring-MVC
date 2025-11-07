51201844423700be732d50d943eeeefc0d1e4eb5
RAJESHREDDY0508 <rajeshreddyguthikonda@gmail.com>
2025-11-07 16:23:54 -0600

    first commit

---
+++ learnmvc/src/main/java/com/rajesh/springmvcboot/HomeController.java
@@ -0,0 +1,46 @@
+package com.rajesh.springmvcboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.rajesh.springmvcboot.model.Alien;
+
+@Controller
+public class HomeController 
+{	
+	@ModelAttribute
+	public void modelData(Model m)
+	{
+		m.addAttribute("name", "Aliens");
+	}
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "index";
+	}
+	
+	@RequestMapping("add")
+	public String add(@RequestParam("num1")int i ,@RequestParam("num2")int j, ModelMap m)
+	{
+		
+		int num3 = i + j;
+		
+		m.addAttribute("num3", num3);
+		
+		return "result";
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute("a1") Alien a, Model m)
+	{
+		
+		m.addAttribute("alien", a);
+		
+		return "result";
+	}
+}
---
+++ learnmvc/src/main/webapp/views/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>My JSP Page</title>
+</head>
+<body>
+    Welcome to My JSP Page
+	
+	<form action="addAlien">
+		Enter your id: <input type="text" name="aid"><br>
+		Enter your name: <input type="text" name="aname"><br>
+		<input type="submit">
+	</form>
+</body>
+</html>
