e17d8524c77e31030fcaa6e347a779f99b5282ef
Rajesh Guthikonda <119047880+RAJESHREDDY0508@users.noreply.github.com>
2025-11-07 16:30:49 -0600

    Add files via upload

---
+++ learnmvc/src/main/java/com/rajesh/springmvcboot/LearnmvcApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh.springmvcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnmvcApplication.class, args);
+	}
+
+}
---
+++ learnmvc/src/test/java/com/rajesh/springmvcboot/LearnmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.springmvcboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
